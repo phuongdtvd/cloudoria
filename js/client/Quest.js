09433ac124273c51b44346b46e21e53f4348850b
Nguyen Duy Phuong <s1f102000039@iniad.org>
2023-09-25 02:55:44 +0900

    Add base Quest class

---
+++ js/client/Quest.js
@@ -0,0 +1,24 @@
+class Quest {
+    constructor(name, testFunction, questGiver, rewards){ //Ran once you initalize class
+        this.name = name;
+        this.testFunction = testFunction;
+        this.questGiver = questGiver;
+        this.rewards = rewards;
+    }
+
+    GetQuestName(){
+        return this.name;
+    }
+
+    GetQuestGive(){
+        return this.questGiver;
+    }
+
+    getRewards(){
+        return this.rewards;
+    }
+
+    GetQuestCompletion(){
+        return this.testFunction();
+    }
+}
