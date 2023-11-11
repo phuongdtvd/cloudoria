5140513fd70b4fd47b3370dd0f3737d5bf1e2ecf
Nguyen Duy Phuong <68218416+phuongdtvd@users.noreply.github.com>
2023-11-12 02:02:56 +0900

    Separate questcheck into a function

--- src/js/Being.js
+++ src/js/Being.js
@@ -231,20 +231,24 @@ function isValidHttpUrl(string) {
     return url.protocol === "http:" || url.protocol === "https:";
   }
 
+async function checkQuest(name, params){
+    return await fetch('http://localhost:8081/game-test', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ "name": name, "params":params }),
+    });
+}
+
 Being.prototype.finishMovement = async function(finalOrientation,action){
     // Called whenever a path has been travelled to its end; based on the action object, the appropriate action is taken
     // finalOrientation is a value between 1 and 4 indicatinh the orientation the player should have at the end of the path
     // action is a small object containing data about what to do once the path is ended (talk to NPC, fight monster, ...)
     if(this.isPlayer) {
         if (action.action == 1) { // talk
-            const response = await fetch('http://localhost:8081/game-test', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ "name": "Quest1", "params":[] }),
-              });
-            console.log(response)
+            // const response = await checkQuest("Quest1", [])
+            // console.log(response)
             if(isValidHttpUrl(action.text)){
                 action.character.displayBubble(action.text, action.text);
             } else{
