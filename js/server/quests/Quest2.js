3934e217f043942ad709fa3d4a9092b44825b077
Nguyen Duy Phuong <s1f1010000239@iniad.org>
2023-11-28 11:09:16 +0900

    Fix state

--- js/server/quests/Quest2.js
+++ js/server/quests/Quest2.js
@@ -1,6 +1,5 @@
 var Quest = require("../Quest")
 var Quest2_test = require("../task_tests/Quest2_test")
 
-var Quest2 = new Quest("Quest1", Quest2_test, "Sorcerer", "sword2")
-
+var Quest2 = new Quest("Quest1", Quest2_test, "Sorcerer", "sword2", "QUEST_2_FAIL", "QUEST_2_DONE")
 module.exports = Quest2
